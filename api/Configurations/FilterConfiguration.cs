a62fd967ad91f7b55d71cb225ac16f32a06d0e84
BikeFix Developer <dev@bikefix.local>
2026-09-14 15:06:59 +0300

    chore: remove AddGlobalFilters method (dead code)
    AddGlobalFilters() was never called in Program.cs.
    Global exception filter registration is already handled by ApiConfiguration.cs.
    
    Simplified FilterConfiguration to only register filters as DI services.

--- api/Configurations/FilterConfiguration.cs
+++ api/Configurations/FilterConfiguration.cs
@@ -18,17 +18,5 @@ public static class FilterConfiguration
         return services;
     }
 
-    public static IServiceCollection AddGlobalFilters(this IServiceCollection services)
-    {
-        services.AddControllers(options =>
-        {
-            // Add global exception filter only
-            options.Filters.Add<GlobalExceptionFilter>();
-            
-            // Validation is handled by InvalidModelStateResponseFactory in ApiConfiguration
-            // Other filters (LoggingFilter, RateLimitingFilter, ApiKeyAuthorizationFilter) are applied selectively via attributes
-        });
 
-        return services;
-    }
 }
